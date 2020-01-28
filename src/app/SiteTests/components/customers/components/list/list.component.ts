2e4677ae566fb050386b9e8c98d6c92cb5f32bee
Sivan Wolberg <Sivan@wolberg.pro>
2020-01-28 23:47:25 +0200

    multi bug fix

--- src/app/SiteTests/components/customers/components/list/list.component.ts
+++ src/app/SiteTests/components/customers/components/list/list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ChangeDetectorRef, ViewChild, OnDestroy } from "@angular/core";
+import {
+  Component,
+  AfterViewInit,
+  ChangeDetectorRef,
+  ViewChild,
+  OnDestroy
+} from "@angular/core";
 import { Observable, merge, Subject } from "rxjs";
 import { MatDialog } from "@angular/material/dialog";
 import {
@@ -8,53 +14,75 @@ import {
 import { Customer } from "../../models/customer.model";
 import { Sort } from "@angular/material/sort";
 import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
-import { startWith, switchMap, map, takeUntil } from "rxjs/operators";
+import { startWith, switchMap, map, takeUntil, filter } from "rxjs/operators";
 import { NgxSpinnerService } from "ngx-spinner";
-import { CustomerFormComponent } from '../customer-form/customer-form.component';
+import { CustomerFormComponent } from "../customer-form/customer-form.component";
 
 @Component({
   selector: "app-list",
   templateUrl: "./list.component.html",
   styleUrls: ["./list.component.scss"]
 })
-export class ListComponent implements OnInit , OnDestroy {
-  public displayedColumns: string[] = ["id", "firstName", "lastName","propAction","propsDefined","actions"];
+export class ListComponent implements AfterViewInit, OnInit,OnDestroy {
+  public displayedColumns: string[] = [
+    "id",
+    "firstName",
+    "lastName",
+    "propAction",
+    "propsDefined",
+    "actions"
+  ];
   resultsLength = 0;
   private unsubscribe$ = new Subject();
   public hasDataDisplay = false;
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  dataSource = new MatTableDataSource<Customer>();
+  dataSource = null;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   constructor(
     public dialog: MatDialog,
     private customerService: CustomerService,
     private changeDetectorRef: ChangeDetectorRef,
     private spinner: NgxSpinnerService
   ) {}
-
   ngOnInit() {
-    this.paginator.pageIndex = 0;
-    this.sort.sortChange.pipe(
-      takeUntil(this.unsubscribe$)
-    ).subscribe(() => (this.paginator.pageIndex = 0));
-    this.customerService.getReloadCustomerList().pipe(
-      takeUntil(this.unsubscribe$)
-    ).subscribe(() => (this.paginator.pageIndex = 0));
-    merge(this.sort.sortChange, this.paginator.page).pipe(
-      takeUntil(this.unsubscribe$),
-      switchMap(() => {
-        this.spinner.show();
-        return this.customerService.getList(
-          this.sort.active,
-          (this.sort.direction === "desc") ? SortDirectorion.DESC : SortDirectorion.ASC,
-          this.paginator.pageSize,
-          this.paginator.pageIndex);
-      }),
-      map(data => {
-        this.spinner.hide();
-        return data;
-      })
-    ).subscribe(data => this.dataSource.data = data);
+    // This line of code below:
+    this.dataSource = new MatTableDataSource<Customer>();
+  }
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.sort.sortChange
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() => (this.paginator.pageIndex = 0));
+    this.customerService
+      .getReloadCustomerList()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() => (this.paginator.pageIndex = 0));
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        switchMap(() => {
+          this.spinner.show();
+          return this.customerService.getList(
+            this.sort.active,
+            this.sort.direction === "desc"
+              ? SortDirectorion.DESC
+              : SortDirectorion.ASC,
+            this.paginator.pageSize,
+            this.paginator.pageIndex
+          );
+        }),
+        map(data => {
+          this.spinner.hide();
+
+          if (!this.dataSource) {
+            return data;
+          }
+          return null;
+        }),
+        filter(data => !!data)
+      )
+      .subscribe(data => (this.dataSource.data = data));
   }
 
   ngOnDestroy(): void {
@@ -63,15 +91,16 @@ export class ListComponent implements OnInit , OnDestroy {
   }
 
   OnRemoveCustomer(customer: Customer) {
-    this.customerService.delete(customer.id).pipe(
-      takeUntil(this.unsubscribe$)
-    ).subscribe(resulved => {
-      if (resulved) {
-        this.paginator.pageIndex = 0; //attempt to reload
-      } else {
-        alert("Unknown error");
-      }
-    });
+    this.customerService
+      .delete(customer.id)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(resulved => {
+        if (resulved) {
+          this.paginator.pageIndex = 0; //attempt to reload
+        } else {
+          alert("Unknown error");
+        }
+      });
   }
   public OnEditCustomer(customer: Customer) {
     this.handleFormDisalog(customer);
@@ -87,21 +116,22 @@ export class ListComponent implements OnInit , OnDestroy {
       data: customer
     });
 
-    dialogRef.afterClosed().pipe(
-      takeUntil(this.unsubscribe$)
-    ).subscribe((result: {valid: boolean , data: Customer}) => {
-      if (result.valid) {
-        this.customerService.edit(customer.id,result.data).pipe(
-          takeUntil(this.unsubscribe$)
-        ).subscribe(resulved => {
-          if (resulved) {
-            this.customerService.reloadCustomerList();
-          } else {
-            alert("Unknown error");
-          }
-        });
-      }
-
-    });
+    dialogRef
+      .afterClosed()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((result: { valid: boolean; data: Customer }) => {
+        if (result.valid) {
+          this.customerService
+            .edit(customer.id, result.data)
+            .pipe(takeUntil(this.unsubscribe$))
+            .subscribe(resulved => {
+              if (resulved) {
+                this.customerService.reloadCustomerList();
+              } else {
+                alert("Unknown error");
+              }
+            });
+        }
+      });
   }
 }
